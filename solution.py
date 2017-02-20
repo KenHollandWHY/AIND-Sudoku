2f02253fcdd9c9fae6042aa9c816a8484abe69d3
KenHollandWHY <dgajdamaschko@gmail.com>
2017-02-19 21:14:47 -0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,18 @@
 assignments = []
 
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diag1 = [[a[0]+a[1] for a in zip(rows,cols)]]
+diag2 = [[a[0]+a[1] for a in zip(rows,cols[::-1])]]
+unitlist = row_units + column_units + square_units +diag1 + diag2
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
